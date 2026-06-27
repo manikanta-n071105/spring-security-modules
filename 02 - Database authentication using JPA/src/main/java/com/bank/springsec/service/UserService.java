0d31fd7607b93487617d876386b22308b7011462
Manikanta <nmanikanta792143@gmail.com>
2026-06-27 17:20:30 +0530

    02 - Database authentication using JPA

---
+++ 02 - Database authentication using JPA/src/main/java/com/bank/springsec/service/UserService.java
@@ -0,0 +1,31 @@
+package com.bank.springsec.service;
+
+import com.bank.springsec.entity.Users;
+import com.bank.springsec.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = repository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found!"));
+
+        return User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
